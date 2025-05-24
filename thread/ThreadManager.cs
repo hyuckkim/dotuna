f0b35371150d4a9f9c40b5b29d83a31a7f5ccf0c
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-05-24 17:46:54 +0900

    using

--- thread/ThreadManager.cs
+++ thread/ThreadManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Data;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
