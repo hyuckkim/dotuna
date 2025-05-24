0535f24b73c31631c0bbfc44d45555fd2fc0564f
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-05-24 03:21:44 +0000

    use dotnet framework

---
+++ Form1.Designer.cs
@@ -0,0 +1,11 @@
+namespace Dotuna
+{
+    partial class Form1
+    {
+        void InitializeComponent()
+        {
+            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 200);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows.Forms;
+
+namespace Dotuna
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main() => Application.Run(new Form1());
+    }
+}
