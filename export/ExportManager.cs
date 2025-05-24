84aad1b24625f3fe45412978f735f3bd3a78fa87
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-05-24 10:27:51 +0000

    to export manager

--- export/ExportManager.cs
+++ export/ExportManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Data;
+using System.Data.DataSetExtensions;
 using System.Linq;
 using System.Text;
 using System.Threading;
