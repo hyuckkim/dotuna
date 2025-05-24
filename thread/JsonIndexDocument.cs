cdc90f60990302e826ea1d80bed1f67d2b872ea9
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-05-24 03:41:18 +0000

    fix dotuna's assembly references

--- thread/JsonIndexDocument.cs
+++ thread/JsonIndexDocument.cs
@@ -1,38 +1,38 @@
 using System.ComponentModel;
 
-namespace DoTuna.Thread;
-
-public class JsonIndexDocument: INotifyPropertyChanged
+namespace DoTuna.thread
 {
-#pragma warning disable IDE1006 // Naming Styles: Because the JSON properties are in snake_case, we need to disable this rule for the properties.
-    public string version { get; set; } = "";
-    public string boardId { get; set; } = "";
-    public int threadId { get; set; } = 0;
-    public string title { get; set; } = "";
-    public string username { get; set; } = "";
-    public DateTime createdAt { get; set; } = new DateTime(0);
-    public DateTime updatedAt { get; set; } = new DateTime(0);
-    public int size { get; set; } = 0;
-#pragma warning restore IDE1006 // Naming Styles
-    public override string ToString()
+    public class JsonIndexDocument: INotifyPropertyChanged
     {
-        return $"{title} /  Username: {username} {threadId}";
-    }
-
-    public bool IsCheck
-    {
-        get => _isCheck;
-        set
+    #pragma warning disable IDE1006 // Naming Styles: Because the JSON properties are in snake_case, we need to disable this rule for the properties.
+        public string version { get; set; } = "";
+        public string boardId { get; set; } = "";
+        public int threadId { get; set; } = 0;
+        public string title { get; set; } = "";
+        public string username { get; set; } = "";
+        public DateTime createdAt { get; set; } = new DateTime(0);
+        public DateTime updatedAt { get; set; } = new DateTime(0);
+        public int size { get; set; } = 0;
+    #pragma warning restore IDE1006 // Naming Styles
+        public override string ToString()
         {
-            _isCheck = value;
-            OnPropertyChanged(nameof(IsCheck));
+            return $"{title} /  Username: {username} {threadId}";
+        }
+
+        public bool IsCheck
+        {
+            get => _isCheck;
+            set
+            {
+                _isCheck = value;
+                OnPropertyChanged(nameof(IsCheck));
+            }
+        }
+        private bool _isCheck = false; // for DataGrid
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-    private bool _isCheck = false; // for DataGrid
-    public event PropertyChangedEventHandler? PropertyChanged;
-    protected void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
 }
