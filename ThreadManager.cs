8b3fbacc7b579ac78b258d2c4ea77bc601c13e46
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-06-02 10:16:09 +0000

    change checked method func

--- ThreadManager.cs
+++ ThreadManager.cs
@@ -10,8 +10,10 @@ namespace DoTuna
     public class ThreadManager
     {
         private IIndexRepository _indexRepository;
+        private HashSet<JsonIndexDocument> _checked = new HashSet<JsonIndexDocument>();
+
         public IEnumerable<JsonIndexDocument> All { get => _indexRepository.Get(); }
-        public IEnumerable<JsonIndexDocument> Checked { get => All.Where(x => x.IsCheck).OrderBy(x => x.threadId); }
+        public IEnumerable<JsonIndexDocument> Checked { get => _checked.OrderBy(x => x.threadId); }
         public IEnumerable<JsonIndexDocument> Filtered(string title, string author)
         {
             return All.Where(x => x.title.Contains(title) &&
@@ -26,6 +28,15 @@ namespace DoTuna
         {
             if (doc == null) return;
             doc.IsCheck = isCheck;
+
+            if (isCheck)
+            {
+                _checked.Add(doc); // 이미 존재하면 추가되지 않음
+            }
+            else
+            {
+                _checked.Remove(doc);
+            }
         }
     }
 }
