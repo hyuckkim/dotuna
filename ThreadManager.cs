96bc958cba894194f90aada9a7a73326d477111f
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-06-03 01:01:31 +0000

    use cache on threadmanager filter

--- ThreadManager.cs
+++ ThreadManager.cs
@@ -12,17 +12,41 @@ namespace DoTuna
         private IIndexRepository _indexRepository;
         private HashSet<JsonIndexDocument> _checked = new HashSet<JsonIndexDocument>();
 
-        public IEnumerable<JsonIndexDocument> All { get => _indexRepository.Get(); }
-        public IEnumerable<JsonIndexDocument> Checked { get => _checked.OrderBy(x => x.threadId); }
+        private List<JsonIndexDocument> _titleCache = null;
+        private List<JsonIndexDocument> _authorCache = null;
+        private string _titleFilter = string.Empty;
+        private string _authorFilter = string.Empty;
+
+        public IEnumerable<JsonIndexDocument> All => _indexRepository.Get();
+        public IEnumerable<JsonIndexDocument> Checked => _checked.OrderBy(x => x.threadId);
         public IEnumerable<JsonIndexDocument> Filtered {
-            get => All.Where(x =>
-                x.title.Contains(TitleFilter) &&
-                x.username.Contains(AuthorFilter)
-            );
+            get {
+                if (_titleCache == null)
+                    _titleCache = All.Where(x => x.title.Contains(_titleFilter)).ToList();
+                if (_authorCache == null)
+                    _authorCache = All.Where(x => x.username.Contains(_authorFilter)).ToList();
+                return _titleCache.Intersect(_authorCache);
+            }
         }
 
-        public string TitleFilter { get; set; } = string.Empty;
-        public string AuthorFilter { get; set; } = string.Empty;
+        public string TitleFilter {
+            get => _titleFilter;
+            set {
+                if (_titleFilter != value) {
+                    _titleFilter = value;
+                    _titleCache = null;
+                }
+            }
+        }
+        public string AuthorFilter {
+            get => _authorFilter;
+            set {
+                if (_authorFilter != value) {
+                    _authorFilter = value;
+                    _authorCache = null;
+                }
+            }
+        }
 
         public ThreadManager(IIndexRepository indexRepository)
         {
