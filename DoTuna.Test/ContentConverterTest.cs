d4e99f858064f2fa89838fd2aca4e36f0e1b73b8
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-06-08 02:25:30 +0000

    test one by one

--- DoTuna.Test/ContentConverterTest.cs
+++ DoTuna.Test/ContentConverterTest.cs
@@ -18,20 +18,8 @@ namespace DoTuna.Test
 
         [Theory]
         [InlineData(
-            ">>123>>45", 
-            "<a href=\"123.html#response_45\">>>123>>45</a>")]
-        [InlineData(
-            "https://bbs.tunaground.net/trace.php/b/123/45", 
-            "<a href=\"123.html#response_45\" target=\"_blank\">https://bbs.tunaground.net/trace.php/b/123/45</a>")]
-        [InlineData(
-            "https://tunaground.co/card2?post/trace.php/b/123/45", 
-            "<a href=\"123.html#response_45\" target=\"_blank\">https://tunaground.co/card2?post/trace.php/b/123/45</a>")]
-        [InlineData(
-            "https://tunaground.co/card2?post/trace.php\\?bbs=b&amp;card_number=123", 
-            "<a href=\"123.html\" target=\"_blank\">https://tunaground.co/card2?post/trace.php\\?bbs=b&amp;card_number=123</a>")]
-        [InlineData(
-            "https://example.com/abc/123", 
-            "<a href=\"https://example.com/abc/123\" target=\"_blank\">https://example.com/abc/123</a>")]
+            "&gt;&gt;7", 
+            "<a href=\"123.html#response_7\">&gt;&gt;7</a>")]
         public void ConvertContent_ReplacesLinks(string input, string expected)
         {
             var converter = GetConverter();
