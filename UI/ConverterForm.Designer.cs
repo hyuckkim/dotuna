03220ffa5e31b3e431aeaa6e8ff124ffa02426fd
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-06-11 10:20:38 +0000

    filling

--- UI/ConverterForm.Designer.cs
+++ UI/ConverterForm.Designer.cs
@@ -18,62 +18,33 @@ namespace DoTuna
             // 
             // txtMain
             // 
-            this.txtMain.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom)
-            | AnchorStyles.Left)
-            | AnchorStyles.Right)));
-            this.txtMain.Location = new System.Drawing.Point(12, 12);
+            this.txtMain.Dock = DockStyle.Fill;
             this.txtMain.Multiline = true;
             this.txtMain.Name = "txtMain";
             this.txtMain.ScrollBars = ScrollBars.Vertical;
-            this.txtMain.Size = new System.Drawing.Size(360, 180);
-            this.txtMain.TabIndex = 0;
             // 
             // panelBottom
             // 
-            this.panelBottom.Anchor = ((AnchorStyles)(((AnchorStyles.Bottom | AnchorStyles.Left)
-            | AnchorStyles.Right)));
-            this.panelBottom.Location = new System.Drawing.Point(0, 200);
+            this.panelBottom.Dock = DockStyle.Bottom;
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(384, 50);
-            this.panelBottom.TabIndex = 3;
-            this.panelBottom.SuspendLayout();
             // 
             // txtAddress
             // 
-            this.txtAddress.Anchor = ((AnchorStyles)(((AnchorStyles.Bottom | AnchorStyles.Left)
-            | AnchorStyles.Right)));
-            this.txtAddress.Location = new System.Drawing.Point(12, 215);
+            this.txtAddress.Dock = DockStyle.Left;
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(260, 23);
-            this.txtAddress.TabIndex = 1;
             this.txtAddress.Text = "https://example.com";
             // 
             // btnConvert
             // 
-            this.btnConvert.Anchor = ((AnchorStyles)((AnchorStyles.Bottom | AnchorStyles.Right)));
-            this.btnConvert.Location = new System.Drawing.Point(285, 213);
+            this.btnConvert.Dock = DockStyle.Right;
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(87, 27);
-            this.btnConvert.TabIndex = 2;
             this.btnConvert.Text = "변환!";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            // 
-            // ConverterForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtMain);
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "ConverterForm";
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Converter";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            this.Text = "DoTuna - 텍스트 링크 변환";
         }
     }
 }
