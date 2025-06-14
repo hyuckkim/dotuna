0afe004ba0dd3d55107039a21df1db229aa663f5
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-06-14 03:18:41 +0000

    add text changes to raw anchors

--- Services/ContentConverterToText.cs
+++ Services/ContentConverterToText.cs
@@ -15,6 +15,7 @@ namespace DoTuna
             if (string.IsNullOrEmpty(content)) return string.Empty;
 
             content = FixBr(content);
+            content = ConvertRawAnchors(content, threadId);
             content = ConvertAnchors(content, threadId);
             content = ConvertTunagroundLinks(content);
             content = ConvertCard2Links(content);
@@ -22,6 +23,24 @@ namespace DoTuna
             content = ConvertArchives(content);
             return content;
         }
+        
+        // >>threadId>>n 앵커 변환 (&gt;가 아니라 > 확인)
+        protected string ConvertRawAnchors(string content, int threadId)
+        {
+            return Regex.Replace(
+                content,
+                @"([a-z]*)>([0-9]*)>([0-9]*)-?([0-9]*)",
+                m => {
+                    var id = m.Groups[2].Value == "" ? threadId.ToString() : m.Groups[2].Value;
+                    var responseStart = m.Groups[3].Value;
+                    if (string.IsNullOrEmpty(id) && string.IsNullOrEmpty(responseStart))
+                        return m.Value;
+                    return MakeAnchorTag(id, responseStart, m.Value, false);
+                },
+                RegexOptions.IgnoreCase
+            );
+        }
+
         protected string ConvertArchives(string content)
         {
             return Regex.Replace(
