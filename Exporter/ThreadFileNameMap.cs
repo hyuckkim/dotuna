32873d9de1c7102ed45645ec501b3bdef05a0bcf
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-07-10 14:44:52 +0000

    try to use threadfilename class

--- Exporter/ThreadFileNameMap.cs
+++ Exporter/ThreadFileNameMap.cs
@@ -4,27 +4,44 @@ using System.IO;
 
 namespace DoTuna
 {
+    public class ThreadFileName
+    {
+        public string ThreadId { get; }
+        public string FileName { get; }
+        public ThreadFileName(JsonIndexDocument doc, string pattern)
+        {
+            ThreadId = doc.threadId.ToString();
+            FileName = ThreadFileNameMap.GetTemplateName(doc, pattern) + ".html";
+        }
+    }
+
     public class ThreadFileNameMap
     {
-        private readonly Dictionary<string, string> _map;
-        public int FileNameCount { get => _map.Values.Distinct().Count(); }
+        private readonly Dictionary<string, ThreadFileName> _map;
+        public int FileNameCount { get => _map.Values.Select(x => x.FileName).Distinct().Count(); }
 
         public ThreadFileNameMap(IEnumerable<JsonIndexDocument> threads)
         {
             _map = threads.ToDictionary(
                 doc => doc.threadId.ToString(),
-                doc => GetTemplateName(doc, Setting.Instance.Pattern) + ".html"
+                doc => new ThreadFileName(doc, Setting.Instance.Pattern)
             );
         }
 
         public string GetFileName(string threadId)
         {
-            return _map.TryGetValue(threadId, out var f) ? f : threadId + ".html";
+            return _map.TryGetValue(threadId, out var f) ? f.FileName : threadId + ".html";
+        }
+
+        public ThreadFileName Get(string threadId)
+        {
+            return _map.TryGetValue(threadId, out var f) ? f : new ThreadFileName(
+                new JsonIndexDocument { threadId = int.Parse(threadId) }, Setting.Instance.Pattern);
         }
 
         public string this[string threadId] => GetFileName(threadId);
 
-        public IReadOnlyDictionary<string, string> AsDictionary() => _map;
+        public IReadOnlyDictionary<string, ThreadFileName> AsDictionary() => _map;
         public static string GetTemplateName(JsonIndexDocument doc, string template)
         {
             if (string.IsNullOrEmpty(template)) return string.Empty;
