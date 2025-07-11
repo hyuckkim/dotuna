5d5d2628df024543706f82944cc934b30edbff50
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-07-11 09:45:04 +0000

    try to fix relativepathto

--- Exporter/ThreadFileNameMap.cs
+++ Exporter/ThreadFileNameMap.cs
@@ -22,34 +22,92 @@ namespace DoTuna
             }
         }
     }
-    public class ThreadFileName
+    public class ThreadFileName : IEquatable<ThreadFileName>
     {
         public int ThreadId { get; }
         public string[] FileNameSegments { get; }
         public string FileName => string.Join("/", FileNameSegments);
         public string PathName => string.Join("/", FileNameSegments.SkipLastOne());
+
         public ThreadFileName(JsonIndexDocument doc, string pattern)
         {
             ThreadId = doc.threadId;
             var fileName = ThreadFileNameMap.GetTemplateName(doc, pattern) + ".html";
             FileNameSegments = fileName.Split('/');
         }
+
+        public ThreadFileName(int threadId, string[] fileNameSegments)
+        {
+            ThreadId = threadId;
+            FileNameSegments = fileNameSegments ?? throw new ArgumentNullException(nameof(fileNameSegments));
+        }
+
         public string GetRelativePathTo(ThreadFileName target)
         {
             var from = this.FileNameSegments;
             var to = target.FileNameSegments;
+            if (from == to) return target.FileName;
             int len = Math.Min(from.Length, to.Length);
             int common = 0;
             while (common < len && from[common] == to[common]) common++;
-            var up = Enumerable.Repeat("..", from.Length - common - 1); // -1: 파일명 제외
+            var upCount = Math.Max(0, from.Length - common - 1);
+            var up = Enumerable.Repeat("..", upCount);
             var down = to.Skip(common);
             var rel = up.Concat(down);
             return string.Join("/", rel);
         }
+
         public string GetRelativePathToRoot()
         {
             if (FileNameSegments.Length <= 1) return "";
             return string.Join("/", Enumerable.Repeat("..", FileNameSegments.Length - 1));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ThreadFileName o)
+            {
+                return Equals(o);
+            }
+            else return false;
+        }
+
+        public bool Equals(ThreadFileName other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            if (ThreadId != other.ThreadId) return false;
+            if (FileNameSegments.Length != other.FileNameSegments.Length) return false;
+            for (int i = 0; i < FileNameSegments.Length; i++)
+            {
+                if (!string.Equals(FileNameSegments[i], other.FileNameSegments[i], StringComparison.Ordinal))
+                    return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = ThreadId;
+                foreach (var segment in FileNameSegments)
+                {
+                    hash = (hash * 397) ^ (segment != null ? segment.GetHashCode() : 0);
+                }
+                return hash;
+            }
+        }
+
+        public static bool operator ==(ThreadFileName left, ThreadFileName right)
+        {
+            if (ReferenceEquals(left, right)) return true;
+            if (left is null || right is null) return false;
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ThreadFileName left, ThreadFileName right)
+        {
+            return !(left == right);
         }
     }
 
